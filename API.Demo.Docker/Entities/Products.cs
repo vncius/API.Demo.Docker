2d81d08ed8570f4b82bca0750501fce4443ca557
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2022-02-08 13:36:30 -0300

    Ajuste na entidade produto para não obrigar a inclusão do campo ID que é gerado pelo MongoDB

--- API.Demo.Docker/Entities/Products.cs
+++ API.Demo.Docker/Entities/Products.cs
@@ -9,7 +9,7 @@ namespace API.Demo.Docker.Entities
         { 
             [BsonId]
             [BsonRepresentation(BsonType.ObjectId)]
-            public string Id { get; set; }
+            public string? Id { get; set; }
 
             [BsonElement("Name")]
             public string Name { get; set; }
