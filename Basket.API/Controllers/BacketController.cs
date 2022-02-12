817b46a4a67d1ebfb0276cb07949a9d472d12a56
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2022-02-11 21:34:35 -0300

    Implementado repository e controlador da API Basket

---
+++ Basket.API/Controllers/BacketController.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BacketController : Controller
+    {
+        private readonly IBasketRepository _repository;
+
+        public BacketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        { 
+            var basket = await _repository.GetBasket(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return StatusCode(202);
+        }
+    }
+}
